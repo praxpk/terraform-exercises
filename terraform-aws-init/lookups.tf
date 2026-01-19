9c08fc4c96be83b536e5193d474726d46a4eead6
praxpk <23168694+praxpk@users.noreply.github.com>
2026-01-19 02:51:20 -0500

    terraform first aws project

---
+++ terraform-aws-init/lookups.tf
@@ -0,0 +1,26 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  owners = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
